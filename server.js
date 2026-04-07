5481558358d9c1e4db19a17771d33107cc5ce3fa
Swapnil <soniswapnil4612@gmail.com>
2026-04-07 08:28:08 +0530

    Add missing /track route to server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.get('/flash-sale', (req,res) => res.sendFile(path.join(__dirname,'public','f
 app.get('/about', (req,res) => res.sendFile(path.join(__dirname,'public','about.html')));
 app.get('/contact', (req,res) => res.sendFile(path.join(__dirname,'public','contact.html')));
 app.get('/product', (req,res) => res.sendFile(path.join(__dirname,'public','product.html')));
+app.get('/track', (req,res) => res.sendFile(path.join(__dirname,'public','track.html')));
 app.get('/admin', (req,res) => res.sendFile(path.join(__dirname,'public','admin','login.html')));
 app.get('/admin/dashboard', (req,res) => res.sendFile(path.join(__dirname,'public','admin','dashboard.html')));
 
